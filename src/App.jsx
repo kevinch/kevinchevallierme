6ddff8efbbb8344dd10856c33cff937dc0a4b0f0
Kevin Aldrik Chevallier <kevin.chevallier@mercadolivre.com>
2023-12-15 11:33:21 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+//   const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 style={{textAlign: "left"}}>Hi, hola, oi, salut.
+Kevin here, french frontend developer turned engineering manager and living in Brazil. Yes, I speak (almost) 4 languages, my spanish is in progress since 2022.<br/><br/>
+
+I currently manage a team of developers, front, back and native at Mercado Livre that works on one of the biggest fintech apps in Latin America, Mercado Pago. More about my career path at LinkedIn.<br/><br/>
+
+Here you will find links to several projects I work on or have developed.</h1>
+        
+    </>
+  )
+}
+
+export default App
