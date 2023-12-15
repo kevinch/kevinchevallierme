de65330b669a5570d7a0de9428fd2d0edaac3e3e
Kevin Aldrik Chevallier <kevin.chevallier@mercadolivre.com>
2023-12-15 11:34:34 -0300

    Add line break

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 style={{textAlign: "left"}}>Hi, hola, oi, salut.
+      <h1 style={{textAlign: "left"}}>Hi, hola, oi, salut.<br/>
 Kevin here, french frontend developer turned engineering manager and living in Brazil. Yes, I speak (almost) 4 languages, my spanish is in progress since 2022.<br/><br/>
 
 I currently manage a team of developers, front, back and native at Mercado Livre that works on one of the biggest fintech apps in Latin America, Mercado Pago. More about my career path at LinkedIn.<br/><br/>
