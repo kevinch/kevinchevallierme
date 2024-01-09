301d27e2f16fb60b41bb15467d9ef4344b6e1c1d
Kevin Aldrik Chevallier <kevin.chevallier@mercadolivre.com>
2024-01-09 14:01:28 -0300

    Fix date

--- src/components/Updates.jsx
+++ src/components/Updates.jsx
@@ -8,7 +8,7 @@ function Updates() {
       <div className="updates-list">
       <Update
           description="Adding Books section. Things I read that might be of interest for others. From 2024 onwards."
-          date="jan 29, 2024"
+          date="jan 9, 2024"
         />
 
         <Update
