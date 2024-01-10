a513a062c72538303b2922ab4a31b7e3e8a74859
Kevin Aldrik Chevallier <kevin.chevallier@mercadolivre.com>
2024-01-10 08:39:15 -0300

    Intro i18n fixes

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,17 +6,17 @@ function Intro() {
         Hi, hola, oi, salut.
         <br />
         <br />
-        Kevin here, french frontend developer turned engineering manager and
-        living in Rio de Janeiro, Brazil.
+        Kevin here, french frontend developer turned engineering manager living
+        in Rio de Janeiro, Brazil.
         <br />
-        <br />I currently manage a team of developers, front, back and native at
-        Mercado Livre that work on one of the biggest fintech apps in Latin
+        <br />I currently manage a team of developers, front, back, and native
+        at Mercado Livre who work on one of the biggest fintech apps in Latin
         America, Mercado Pago. More about my career path at{" "}
         <a href="https://www.linkedin.com/in/kevinchevallier/">LinkedIn</a>.
         <br />
         <br />
         Here you will find links to several projects I work on or have developed
-        and some frontend experiments.
+        and some extra bits.
       </div>
       <span className="code-help">{"</Intro>"}</span>
     </div>
