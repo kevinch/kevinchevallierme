99f644b4d6db019c3e2a1ab2345258d054f2f2eb
Kevin Aldrik Chevallier <kevin.chevallier@mercadolivre.com>
2024-01-10 08:27:23 -0300

    Add Intro component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Intro from "./components/Intro.jsx";
 import Books from "./components/Books.jsx";
 import Projects from "./components/Projects.jsx";
 import Updates from "./components/Updates.jsx";
@@ -7,31 +8,9 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <div className="intro-container container">
-        <span className="code-help">{"<Intro>"}</span>
-        <div className="intro-text">
-          Hi, hola, oi, salut.
-          <br />
-          <br />
-          Kevin here, french frontend developer turned engineering manager and
-          living in Rio de Janeiro, Brazil.
-          <br />
-          <br />I currently manage a team of developers, front, back and native
-          at Mercado Livre that work on one of the biggest fintech apps in Latin
-          America, Mercado Pago. More about my career path at{" "}
-          <a href="https://www.linkedin.com/in/kevinchevallier/">LinkedIn</a>.
-          <br />
-          <br />
-          Here you will find links to several projects I work on or have
-          developed and some frontend experiments. 
-        </div>
-        <span className="code-help">{"</Intro>"}</span>
-      </div>
-
+      <Intro />
       <Projects />
-
       <Books />
-
       <Updates />
     </div>
   );
---
+++ src/components/Intro.jsx
@@ -0,0 +1,26 @@
+function Intro() {
+  return (
+    <div className="intro-container container">
+      <span className="code-help">{"<Intro>"}</span>
+      <div className="intro-text">
+        Hi, hola, oi, salut.
+        <br />
+        <br />
+        Kevin here, french frontend developer turned engineering manager and
+        living in Rio de Janeiro, Brazil.
+        <br />
+        <br />I currently manage a team of developers, front, back and native at
+        Mercado Livre that work on one of the biggest fintech apps in Latin
+        America, Mercado Pago. More about my career path at{" "}
+        <a href="https://www.linkedin.com/in/kevinchevallier/">LinkedIn</a>.
+        <br />
+        <br />
+        Here you will find links to several projects I work on or have developed
+        and some frontend experiments.
+      </div>
+      <span className="code-help">{"</Intro>"}</span>
+    </div>
+  );
+}
+
+export default Intro;
