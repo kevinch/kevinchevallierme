faf2f4c31bfc7a76082ca275396195a6f4efa7a8
kevin <kevinchevallier@gmail.com>
2024-05-15 09:00:05 -0300

    Update book link

--- components/Books.tsx
+++ components/Books.tsx
@@ -11,7 +11,7 @@ function Books() {
           pages={2598}
           language="french"
           date="may 2024"
-          link="https://www.amazon.fr/Mis%C3%A9rables-Victor-Hugo-%C3%89dition-Int%C3%A9grale/dp/B0C9KTPQTS/ref=sr_1_8?crid=2BJOSZ0QLIY3G&dib=eyJ2IjoiMSJ9.rB49g7AvpQ_5c8FhjBirEyd6Klm6Ddb90YG0FNCKCXNWXDWPEVZUQn8zDw-gkXksWDl3q_Gi1RYI1a1VcpSs2sbMNLROdk5MBFciUn3IKDI7iIamFdS624kBrk-CCIoUwJGUVESjr2GQeL-MCMRSY5KD_Dv_a33e3APeuRIiqmR1MLngfPXaUJ7xnXSQqE2KH7CdU_B5vlO9sGdODhaAXSjdmrF5PSKNkXyhLDpYO7-LUpaEpxuAhnL_kA5HxLWRuy1_tZPzdY8E-5yuxpj1r-bVZT9u1nbpxAidRrkqfcE.TjepGqa1Su3Br-rY7PpJB3-78aEYo_J5cYJ2fQ7o-jc&dib_tag=se&keywords=les+miserables+victor+hugo&qid=1715686576&sprefix=les+miserables%2Caps%2C333&sr=8-8"
+          link="https://fr.wikipedia.org/wiki/Les_Mis%C3%A9rables"
         />
 
         <Book
